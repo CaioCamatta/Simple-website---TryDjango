eb7802cfea224cfe69a58b419449fd7d6f7a9832
Caio Camatta <caiocamattacoelho@hotmail.com>
2016-06-09 18:07:50 -0300

    Changes on allowed hosts to fix 500

--- website/settings/production.py
+++ website/settings/production.py
@@ -180,7 +180,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
