1ac7c3ca33e67136485e61060c52b80aeec6fc98
Caio Camatta <caiocamattacoelho@hotmail.com>
2016-06-08 22:02:17 -0300

    updated settins

--- website/settings/production.py
+++ website/settings/production.py
@@ -166,7 +166,7 @@ LOGIN_REDIRECT_URL = "/"
 
 
 # Update database configuration with $DATABASE_URL.
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
