8c1694e317396aa238e46a60d5c8973cb0a88c44
Caio Camatta <caiocamattacoelho@hotmail.com>
2016-06-08 21:43:47 -0300

    settings updated

--- website/settings/production.py
+++ website/settings/production.py
@@ -11,7 +11,7 @@ TEMPLATE_DEBUG = True
 DATABASES = settings.DATABASES
 
 # Update database configuration with $DATABASE_URL.
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
