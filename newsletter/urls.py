f56065c5d1c8fe32cf3798aad2cbf8494d10756e
Caio Camatta <caiocamattacoelho@hotmail.com>
2016-06-10 15:28:07 -0300

    Trying to fix homepage error

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^contact/$', views.home, name='home'),
-    url(r'^$', views.contact, name='contact'),
+    url(r'^$', views.home, name='home'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^about/$', views.about, name='about'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -10,44 +10,44 @@ from .models import SignUp
 
 # Requests are what you request from the server. The server return something back.
 def home(request):
-    title = "Welcome"
-    # request.POST runs through the validators, if there's no data it doesnt run through it
-    # if request.POST is true (in this case if you press the button), use the POST information in the validators,
-    # if its false, don't use any info(which means that it will not run through the validators)
-    # the POST info, here, is the full_name and the email
-    form = SignUpForm(request.POST or None)
-    context = {
-        "template_title": title,
-        "form": form,
-    }
+    # title = "Welcome"
+    # # request.POST runs through the validators, if there's no data it doesnt run through it
+    # # if request.POST is true (in this case if you press the button), use the POST information in the validators,
+    # # if its false, don't use any info(which means that it will not run through the validators)
+    # # the POST info, here, is the full_name and the email
+    # form = SignUpForm(request.POST or None)
+    # context = {
+    #     "template_title": title,
+    #     "form": form,
+    # }
+    #
+    # # checks if the given data is valid, if the model is valid, like making sure the name is/not blank, etc
+    # if form.is_valid():
+    #     # sets variable but doesnt save it. allows us to do something before saving
+    #     instance = form.save(commit=False)
+    #     # sets variable 'full_name' to be the full_name data from the form
+    #     full_name = form.cleaned_data.get("full_name")
+    #     # if its empty, sets the var above^ to the string, else leave as given
+    #     if not full_name:
+    #         full_name = "New Full Name"
+    #     # sets the full_name from the form to the full_name var.
+    #     instance.full_name = full_name
+    #     instance.save()
+    #     # after completing the forms, change the context
+    #     context = {
+    #         "template_title": "Success, " + instance.full_name,
+    #     }
+    # if request.user.is_authenticated() and request.user.is_staff:
+    #     # print(SignUp.objects.all().all())
+    #     # for instance in SignUp.objects.all():
+    #     #    print(instance.full_name)
+    #     queryset = SignUp.objects.all().order_by('-timestamp')
+    #     #.filter(full_name__icontains="Caio Camatta")
+    #     context = {
+    #         "queryset": queryset,
+    #     }
 
-    # checks if the given data is valid, if the model is valid, like making sure the name is/not blank, etc
-    if form.is_valid():
-        # sets variable but doesnt save it. allows us to do something before saving
-        instance = form.save(commit=False)
-        # sets variable 'full_name' to be the full_name data from the form
-        full_name = form.cleaned_data.get("full_name")
-        # if its empty, sets the var above^ to the string, else leave as given
-        if not full_name:
-            full_name = "New Full Name"
-        # sets the full_name from the form to the full_name var.
-        instance.full_name = full_name
-        instance.save()
-        # after completing the forms, change the context
-        context = {
-            "template_title": "Success, " + instance.full_name,
-        }
-    if request.user.is_authenticated() and request.user.is_staff:
-        # print(SignUp.objects.all().all())
-        # for instance in SignUp.objects.all():
-        #    print(instance.full_name)
-        queryset = SignUp.objects.all().order_by('-timestamp')
-        #.filter(full_name__icontains="Caio Camatta")
-        context = {
-            "queryset": queryset,
-        }
-
-    return render(request, "newsletter/home.html", context)
+    return render(request, "newsletter/home.html", {})
 
 
 def contact(request):
