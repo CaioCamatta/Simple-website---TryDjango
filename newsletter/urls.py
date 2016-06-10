080d790fdda66d1d020cd04a24e6cd52953f4567
Caio Camatta <caiocamattacoelho@hotmail.com>
2016-06-10 15:05:03 -0300

    Trying to fix homepage error

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^about/$', views.about, name='about'),
 ]
